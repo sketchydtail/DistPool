eb814d5ad4b6cd810248244a0c944316ec6e0d1c
King Joolian <king_joolian@internode.on.net>
2016-02-21 16:21:13 +1030

    Added copyright notice

--- src/DistPool/DistPoolExecutor.java
+++ src/DistPool/DistPoolExecutor.java
@@ -1,9 +1,17 @@
-/**
- * Distributed Thread Pool main class.
+/*
+ * Copyright (c) 2016. Julian Hunt.
  *
- * @author Julian Hunt aka. Sketchy D Tail
- * @version 1.0, 21/02/2016
- * Run pooled tasks on multiple networked machines.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package DistPool;
@@ -31,6 +39,14 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 enum RunState {STARTING, RUNNING, SHUTDOWN, STOP, TIDYING, TERMINATED}
+
+/**
+ * Distributed Thread Pool main class.
+ *
+ * @author Julian Hunt aka. Sketchy D Tail
+ * @version 1.0, 21/02/2016
+ * Run pooled tasks on multiple networked machines.
+ */
 
 public class DistPoolExecutor<T extends DistributableTask> implements MasterRemote, DistributorService {
     private static final Logger logger = Logger.getLogger("DistPool");
